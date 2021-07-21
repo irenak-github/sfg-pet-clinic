92545ab485cd1214e290a00132e53656703b6971
IrenaKinder <irena.kinder@abc-techgroup.com>
2021-07-21 14:49:23 +0200

    Interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/irena/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package irena.springframework.sfgpetclinic.services;
+
+import irena.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/irena/springframework/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package irena.springframework.sfgpetclinic.services;
+
+import irena.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
