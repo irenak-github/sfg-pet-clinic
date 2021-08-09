50ec247a76d66ccb24c313b86702943e259345ca
IrenaKinder <irena.kinder@abc-techgroup.com>
2021-08-09 11:21:57 +0200

    Adding PetType Service. Closes #29.

---
+++ pet-clinic-data/src/main/java/irena/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package irena.springframework.sfgpetclinic.services;
+
+import irena.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
