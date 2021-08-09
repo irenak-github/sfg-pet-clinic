6afce645a9636f597fbd898be95dfb0745831f19
IrenaKinder <irena.kinder@abc-techgroup.com>
2021-08-09 12:08:24 +0200

    Adding Specialities Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/irena/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package irena.springframework.sfgpetclinic.services;
+
+import irena.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
